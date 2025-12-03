f5f5e442c1f87f76aa77cf94c1f2714e26a1fb91
eFerrazz <enzoblindao@gmail.com>
2025-12-03 13:32:17 -0300

    tailwind.config.js somefix1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ export default {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+	'./node_modules/@ferrucc-io/emoji-picker/dist/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
   	container: {
