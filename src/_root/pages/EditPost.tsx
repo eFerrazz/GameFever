6265ebd3c0f007e3a00dd057a279678d15c83411
eFerrazz <enzoblindao@gmail.com>
2025-12-03 13:25:49 -0300

    EditPost.tsx somefix1.1

--- src/_root/pages/EditPost.tsx
+++ src/_root/pages/EditPost.tsx
@@ -3,28 +3,33 @@ import PostForm from '@/components/forms/PostForm'
 import { useGetPostById } from '@/lib/react-query/queriesAndMutations';
 import Loader from '@/components/shared/Loader';
 
-
 const EditPost = () => {
-  
+
   const { id } = useParams();
   const { data: post, isPending } = useGetPostById(id || '');
 
-  if(isPending) return <Loader/ >
+  if(isPending) return <Loader />
 
   return (
-    <div className="flex flex-1">
-      <div className="common-container">
-          <div className="max-w-5x1 flex-start gap-3 justify-start w-full">
-            <img 
-             src="/assets/icons/add-post.svg"
-             width={36}
-             height={36}
-             alt="Adicionar"
-            />
-            <h2 className="h3-bold md:h2-bold text-left w-full">Editar Postagem</h2>
-          </div>
+    <div className="flex flex-1 justify-center">
+      <div className="common-container w-full max-w-5xl p-6 flex flex-col gap-6">
+        
+        {/* Header */}
+        <div className="flex items-center gap-3 w-full">
+          <img 
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="Editar"
+          />
+          <h2 className="h3-bold md:h2-bold text-left w-full">Editar Postagem</h2>
+        </div>
 
+        {/* Form */}
+        <div className="w-full flex flex-col gap-7 mt-4">
           <PostForm action="Update" post={post}/>
+        </div>
+
       </div>
     </div>
   )
